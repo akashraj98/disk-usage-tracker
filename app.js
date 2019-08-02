e202500b02e3e3c19b8e9fa9cd8bd0ac29f04e5d
akashraj98 <akashraj7713@gmail.com>
2019-08-03 00:14:19 +0530

    updated final changes

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const sqliteJson = require('sqlite-json');
 let db = new sqlite3.Database('./log.db');
 const exporter = sqliteJson(db);
 const table = "disklog";
-db.run('CREATE TABLE IF NOT EXISTS '+table+'(HOSTNAME TEXT ,MOUNTPOINT TEXT,TOTALSIZE TEXT,USED TEXT,AVAIL TEXT,PERCENTAGEUSED TEXT)');
-db.get("SELECT HOSTNAME FROM disklog", function(error, row) {
+db.run('CREATE TABLE IF NOT EXISTS '+table+'(HOSTNAME TEXT ,MOUNTPOINT TEXT,TOTALSIZE TEXT,USED TEXT,AVAIL TEXT,PERCENTAGEUSED TEXT);');
+db.get("SELECT * FROM disklog LIMIT 2;", function(error, row) {
     if (row !== undefined) {
         console.log("table exists. cleaning existing records");
         db.run("DELETE FROM disklog", function(error) {
@@ -22,8 +22,8 @@ app.use(bodyParser.json()) // for parsing application/json
 app.post('/post',(req,res)=>{
     try {
 
-        db.run("INSERT INTO disklog(HOSTNAME,MOUNTPOINT,TOTALSIZE,USED,AVAIL,PERCENTAGEUSED) SELECT(?),(?),(?),(?),(?),(?) WHERE NOT EXISTS(SELECT * FROM disklog  WHERE HOSTNAME=(?))",
-        req.body.hostname, req.body.mountpoint,req.body.totalsize,req.body.used,req.body.avail,req.body.percentageused,req.body.hostname);
+        db.run("INSERT INTO disklog(HOSTNAME,MOUNTPOINT,TOTALSIZE,USED,AVAIL,PERCENTAGEUSED) SELECT(?),(?),(?),(?),(?),(?) WHERE NOT EXISTS(SELECT * FROM disklog  WHERE HOSTNAME=(?));",
+        req.body.hostname, req.body.mountPoint,req.body.totalsize,req.body.used,req.body.avail,req.body.percentageused,req.body.hostname);
         res.send("data recieved")
     }catch (error) {    
         console.error('ERROR:');
@@ -35,15 +35,28 @@ app.post('/post',(req,res)=>{
     });
 app.get('/',(req,res)=>{
 
-    exporter.json('select * FROM disklog', function (err, json) {
+    exporter.json('select * FROM disklog;', function (err, json) {
         console.log(json)
         res.send(json)
-        db.run("DELETE FROM disklog", function(error) {
+        db.run("DELETE FROM disklog;", function(error) {
             if (error)
                 console.log(error);
         });
       });
  });
 
-app.listen(5050);
-console.log("running on port 5050");
+//  sample post request3
+//  {
+// 	"hostname": "master-Inspiron",
+// 	"mountPoint": "/user/bin", 
+// 	"totalsize": "31G", 
+// 	"used": "16G", 
+// 	"avail": "14G", 
+// 	"percentageused": "53%"
+	
+// }
+
+
+
+app.listen(8080);
+console.log("running on port 8080");
