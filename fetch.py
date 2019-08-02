2f83bc03270f3bc2b47eeeedfba73aa448c231d9
akashraj98 <akashraj7713@gmail.com>
2019-08-03 00:17:56 +0530

    changed url to hit

--- fetch.py
+++ fetch.py
@@ -14,10 +14,11 @@ def checkOnce():
 if __name__ == "__main__":
     threashold = 9
     path = '/'
+    url = 'http://localhost:8080/post'
     log = checkOnce()
     for disklog in log[0]:
         if path in disklog[5] and len(disklog[-1])==1:
             payload={'hostname':socket.gethostname(),'mountPoint':disklog[-1],'totalsize':disklog[1],'used':disklog[2],
             'avail':disklog[3],'percentageused':disklog[4]}
-            r = requests.post('http://localhost:5047/post', data=json.dumps(payload))#provide with url
+            r = requests.post(url, data=json.dumps(payload))#provide with url
             print(r.text)
