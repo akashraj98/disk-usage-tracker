1e8b93498f24844232c54f3444bbd3ca5e58c078
akashraj98 <akashraj7713@gmail.com>
2019-08-03 00:19:20 +0530

    small change

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose()
 const request = require('request');
+
 const sqliteJson = require('sqlite-json');
 let db = new sqlite3.Database('./log.db');
 const exporter = sqliteJson(db);
